f1afc0e3a9da4e6b1c359fcde12cb0c8c18b4dcd
cyber-country <180425518+cyber-country@users.noreply.github.com>
2026-04-29 14:20:43 +0530

    add clean text structure function

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask,request,render_template
 import pdfplumber
+import re
 def extract(n):
     with pdfplumber.open(n) as pdf:
         text=""
@@ -7,6 +8,9 @@ def extract(n):
         for page in pages:
             text+=(page.extract_text() or "")+"\n"
         return text
+def text_organizer(s):
+    s=re.sub(r'\s+',' ',s)
+    return s
 app=Flask(__name__)
 @app.route("/",methods=["GET","POST"])
 def home():
@@ -15,7 +19,8 @@ def home():
         if file and file.name!="":
             print("file_successfully recieved")
         text = extract(file)
+        clean_text=text_organizer(text)
         print("successfully the pdf operation has been done...")
-        return f"<pre>{text}</pre>"
+        return clean_text
     return render_template("index.html")
 app.run(debug=True)
